4e3ade4bd8997c646894538297e0cf4e5967321a
Michael <michael@fallo.ws>
2016-06-13 22:07:45 +0100

    Fixed possible NULL file issue
    - If NULL file pointer was passed it was not correctly handled
    - Replaced ascii numerical values for single quote versions
    - Tidied up code

--- hexdump.c
+++ hexdump.c
@@ -21,44 +21,54 @@
 #include <unistd.h>
 #include <string.h>
 #define HEXDUMP_ROWLEN 16
-#define HEXDUMP_REPLACECHAR 46
+#define HEXDUMP_REPLACECHAR '.'
 #define HEXDUMP(d,l) HEXDUMP_TO(d,l,stdout)
 void HEXDUMP_TO(void *d,size_t l,FILE *w) __attribute((nonnull));
+
 void HEXDUMP_TO(void *d,size_t l,FILE *w){
-	if(!d || !w){ l= 0; }
-	unsigned char *c= (unsigned char *) (d); /* Current Byte */
-	unsigned char *e= c + (l); /* End Byte */
+	/* If file is NULL, bail */
+	if(!w){ return; }
+
+	/* If data is NULL, treat as length zero */
+	if(!d){ l= 0; }
+
+	unsigned char *c= (unsigned char *) d; /* Current Byte */
+	unsigned char *e= c + l; /* End Byte */
 	unsigned char b[HEXDUMP_ROWLEN+1]= {0}; /* Buffer */
+
+	/* Loop through each char */
 	for(;c < e;c++){
 		/* New Line needed */
-		if(!((c - (unsigned char *) (d)) % HEXDUMP_ROWLEN)){
+		if(!((c - (unsigned char *) d) % HEXDUMP_ROWLEN)){
 			/* If not first line print data from last line */
-			if(c != (unsigned char *) (d)){ fwrite(b,1,HEXDUMP_ROWLEN+1,(w)); }
+			if(c != (unsigned char *) d){ fwrite(b,1,HEXDUMP_ROWLEN+1,w); }
 
 			/* Print offset */
-			fprintf((w),"\n\t0x%04lx:  ",(c - (unsigned char *) (d)));
+			fprintf(w,"\n\t0x%04lx:  ",(c - (unsigned char *) d));
 
 			/* Fill buffer with spaces */
-			memset(b,32,sizeof(b));
+			memset(b,' ',sizeof(b));
 		}
+
 		/* Print hex char */
-		fprintf((w),"%02x",*c);
+		fprintf(w,"%02x",*c);
 
 		/* Add char to print buffer (or a . if non-printable) */
-		b[((c - (unsigned char *) (d)) % HEXDUMP_ROWLEN) + 1]= (*c > 32 && *c < 127?*c:HEXDUMP_REPLACECHAR);
+		b[((c - (unsigned char *) d) % HEXDUMP_ROWLEN) + 1]= (*c > ' ' && *c <= '~'?*c:HEXDUMP_REPLACECHAR);
 
 		/* If second in pair, print trailing space */
-		if(((c - (unsigned char *) (d)) % 2)){ fputc(32,(w)); }
+		if(((c - (unsigned char *) d) % 2)){ fputc(' ',w); }
 	}
+
 	/* Print trailing spaces if we do not have enough bytes */
-	for(;((c - (unsigned char *) (d)) % HEXDUMP_ROWLEN);c++){
-		fputc(32,(w)); fputc(32,(w));
-		if(((c - (unsigned char *) (d)) % 2)){ fputc(32,(w)); }
+	for(;((c - (unsigned char *) d) % HEXDUMP_ROWLEN);c++){
+		fputc(' ',(w)); fputc(' ',w);
+		if(((c - (unsigned char *) d) % 2)){ fputc(' ',w); }
 	}
 
 	/* Print Last line */
-	fwrite(b,1,HEXDUMP_ROWLEN+1,(w));
-	fprintf((w),"\n\t -- %lu bytes from pointer %p --\n",(e-(unsigned char *) (d)),(d));
+	fwrite(b,1,HEXDUMP_ROWLEN+1,w);
+	fprintf(w,"\n\t -- %lu bytes from pointer %p --\n",l,d);
 }
 
 #endif
